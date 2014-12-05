c98ce79cc248f58b76562110895142285233e348
Papa Sax <alexandre.desenfant@gmail.com>
2014-12-05 16:20:52 +0100

    first commit

---
+++ zoumapp/views.py
@@ -0,0 +1,23 @@
+import urllib2
+
+from django.http.response import HttpResponse
+
+
+def home(request):
+    url = "http://alexandredesenfant.wix.com/zoumafrika"
+    if request.method == 'GET':
+        try:
+            result = urllib2.urlopen(url)
+        except urllib2.URLError, e:
+            pass
+        html=result.read()
+        htmlclean=html.replace('<div comp="wysiwyg.viewer.components.WixAds" skin="wysiwyg.viewer.skins.wixadsskins.WixAdsWebSkin" id="wixFooter"></div>','')
+        return HttpResponse(htmlclean)
+    elif request.method == 'POST':
+        try:
+            newrequest = urllib2.Request(dir, request.POST, headers={})
+            response = urllib2.urlopen(newrequest).read()
+        except urllib2.URLError, e:
+            pass
+        return HttpResponse(response)
+
